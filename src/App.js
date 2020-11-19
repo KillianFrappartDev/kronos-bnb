c6a546a17f738d4cabdf1a82e1fb5ecaf5482c77
KillianFrappartDev <killianFrappartDev@Gmail.com>
2020-11-19 18:07:36 +0100

    Fix merge issues

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import DateFnsUtils from '@date-io/date-fns';
 import { createMuiTheme } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import teal from '@material-ui/core/colors/teal';
-import { Switch, Route, Link, Router, withRouter } from "react-router-dom";
+import { Switch, Route, Link, Router, withRouter, useHistory } from 'react-router-dom';
 
 //Local imports
 import HomePage from './pages/HomePage';
-import ResultPage from "./pages/ResultPage";
+import ResultPage from './pages/ResultPage';
 import './styles/App.css';
 
 const darkTheme = createMuiTheme({
@@ -19,30 +19,23 @@ const darkTheme = createMuiTheme({
 });
 
 function App(props) {
-     const [travelData, setTravelData] = useState(null);
-    
-      const handleSubmit = data => {
+  const [travelData, setTravelData] = useState(null);
+  const history = useHistory();
+
+  const handleSubmit = data => {
     setTravelData(data);
+    history.push('/Results');
   };
-    
+
   return (
     <div className="App">
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/Results">Result Page</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
       <Switch>
         <Route exact path="/">
-              <ThemeProvider theme={darkTheme}>
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <HomePage onSubmit={handleSubmit} />
-      </MuiPickersUtilsProvider>
-    </ThemeProvider>
+          <ThemeProvider theme={darkTheme}>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <HomePage onSubmit={handleSubmit} />
+            </MuiPickersUtilsProvider>
+          </ThemeProvider>
         </Route>
         <Route path="/Results">
           <ResultPage />
