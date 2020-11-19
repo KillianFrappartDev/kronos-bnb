3e46b1b6ba625c3b8785476a8da67ca5fc89e07f
Antoine <ant.g@hotmail.be>
2020-11-19 14:56:59 +0100

    Data

---
+++ src/utils/Data.js
@@ -0,0 +1,136 @@
+const bnbData = [
+    {
+        id : 1,
+        title : 'Storming of the Bastille',
+        year : 1789,
+        city: 'Paris',
+        country: 'France',
+        continent: 'Europe',
+        isTaken: true,
+        description : "This studio has been entirely renovated ans is brand new. The studio was a former bellman loge, it is on the ground floor in a small yard. It has 2 windows, wooden walls, high ceilings, a mezzanina for the bed. it is an amzing place to wath the Storming of the Bastille ans all the beheaded people running around. Really Lovely place ",
+        rating: 4.16,
+        image: 'https://i.ebayimg.com/images/g/Vi0AAOSwQXlcPkMx/s-l1600.jpg',
+        
+
+    },
+    {
+        id : 2,
+        title : 'Painting of Mona Lisa',
+        year : 1517,
+        city: 'Florence',
+        country: 'Italie',
+        continent: 'Europe',
+        isTaken: false,
+        description : 'Lovely House in Florence where you the 2nd floor is available. The 1st floor is occupied by the owner Mr. Da Vinci.',
+        rating: 3.89,
+        image: 'https://images.unsplash.com/photo-1590673452623-cf094b5e08f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80',
+
+    },
+    {
+        id : 3,
+        title : 'Tiny House on Mars',
+        year : 2572,
+        city: 'Mars',
+        country: 'Mars',
+        continent: 'Mars',
+        isTaken: false,
+        description : 'Beautiful tiny house en Mars With incredible view on the martian desert. Two minutes away from Elon Musk Penthouse.',
+        rating: 4.5,
+        image: 'https://4.bp.blogspot.com/-nxdDd3Cu9VE/W3BkTi-SRwI/AAAAAAAADEI/d1IQKkIj64M-sC7q9mmasJYUTjz7VbmjwCLcBGAs/s1000/Mars%2Bhouse%2Bexterior%2Bby%2BEvexoian.jpg',
+
+    },
+    {
+        id : 4,
+        title : 'Paleolithic hut',
+        year : -10000,
+        city: 'Nuremberg',
+        country: 'Germany',
+        continent: 'Europe',
+        isTaken: true,
+        description : 'Really nice hut in the forest. Be carrefull at night because of the wolves ... and also the neighbour that kidnapped my wife a couple of time',
+        rating: 2.67,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/5/55/Stone_age_projekt_in_Sweden_7.jpg',
+
+    },
+    {
+        id : 5,
+        title : 'Aztec house',
+        year : 1428,
+        city: 'Guadalajara',
+        country: 'Mexico',
+        continent: 'North America',
+        isTaken: false,
+        description : "Rectangular hut with an open doorway onto the patio, the house backed onto the street. The neighbourhood is quiet except when the Hernán Cortés and his Spanish conquistadors have drinks outside.",
+        rating: 3.9,
+        image: 'https://images.unsplash.com/photo-1551914710-723bdd36c2b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+
+    },
+    {
+        id : 6,
+        title : ' Egyptian Antique house',
+        year : 1634,
+        city: 'Cairo',
+        country: 'Egypte',
+        continent: 'Africa',
+        isTaken: false,
+        description : 'Beautiful house in Egypte with fantastic view with pyramids',
+        rating: 4.48,
+        image: 'https://i.pinimg.com/564x/a9/76/4a/a9764ae4d69299417e24eed61a017f59.jpg',
+
+    },
+    {
+        id : 7,
+        title : 'Flat with view on Berlin Wall',
+        year : 1989,
+        city: 'Berlin',
+        country: 'Germany',
+        continent: 'Europe',
+        isTaken: false,
+        description : "Nice modern flat in front of the Berlin Wall. Perfect place for celebration.",
+        rating: 2.56,
+        image: 'https://images.unsplash.com/photo-1600661991570-ccd9563faaaa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+
+    },
+    {
+        id : 8,
+        title : 'Ryad Bahia',
+        year : 1900,
+        city: 'Marrakech',
+        country: 'Maorocco',
+        continent: 'Africa',
+        isTaken: false,
+        description : " An inspiring place, Ryad Bahia is a place were you live, eat, sleep and relax. A fantastic place with 3 rooms, two minutes walk from the magic square of Jemaa Elfna  ",
+        rating: 4.1,
+        image: 'https://www.palais-bahia.com/wp-content/uploads/2017/11/Palais-Bahia-2.jpg',
+    },
+    {
+        id : 9,
+        title : 'Ottman Empire house',
+        year : 1665,
+        city: 'Istanbul',
+        country: 'Turkey',
+        continent: 'Asie',
+        isTaken: false,
+        description : " A big house with a fantsatic view sea view, just a five minutes to the Blue Mosque and Hagia Sofia  ",
+        rating: 4.23,
+        image: 'https://images.unsplash.com/photo-1578004952492-69c54a10bc1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80https://images.unsplash.com/photo-1578004952492-69c54a10bc1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80
+    },
+    {
+        id : 10,
+        title : 'Birth of Jesus',
+        year : 0,
+        city: 'Nazareth',
+        country: 'Israel',
+        continent: 'Asia',
+        isTaken: false,
+        description : "Welcoming family in Nazareth. The house is not really big but the son washes feet very well.",
+        rating: 3.76,
+        image: 'https://s21858.pcdn.co/wp-content/uploads/2012/12/Recreation.jpg',
+
+    },
+
+]
+
+    
+
+
